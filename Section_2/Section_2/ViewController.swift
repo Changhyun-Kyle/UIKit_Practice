5fe5ff983fb8a6b71f124b4a7b9314c870ab860b
Changhyun Kang <changhyun.kyle@gmail.com>
2023-03-14 15:58:21 +0900

    [Progress] Section 2: I Am Rich

---
+++ Section_2/Section_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Section_2
+//
+//  Created by 강창현 on 2023/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
