032891ad068e107a4e6ced0051ac030665b16995
Changhyun Kang <changhyun.kyle@gmail.com>
2023-03-24 11:27:44 +0900

    [Progress] Section 11: BMI-Calculator

---
+++ Section_11/BMI_Calculator/BMI_Calculator/Model/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI_Calculator
+//
+//  Created by 강창현 on 2023/03/24.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- Section_11/BMI_Calculator/BMI_Calculator/Model/CalculatorBrain.swift
+++ Section_11/BMI_Calculator/BMI_Calculator/Model/CalculatorBrain.swift
@@ -5,19 +5,55 @@
 //  Created by 강창현 on 2023/03/23.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     func getBMIValue() -> String {
-        let bmiToDecimalValue = String(format: "%.1f", bmi)
+        // MARK: - Nil Coalescing Operator
+        let bmiToDecimalValue = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiToDecimalValue
+        // MARK: - Optional Binding
+        /*
+         if let safeBMI = bmi {
+         let bmiToDecimalValue = String(format: "%.1f", safeBMI)
+         return bmiToDecimalValue
+         } else {
+         return "0.0"
+         }
+         */
+        // MARK: - Chek for nil Value with Force Unwrapping
+        /*
+         if bmi != nil {
+         let bmiToDecimalValue = String(format: "%.1f", bmi!)
+         return bmiToDecimalValue
+         } else {
+         return "0.0"
+         }
+         */
     }
     
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No advice"
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
     mutating func caculateBMI(height: Float, weight: Float) {
         let bmiValue = weight / pow(height, 2)
-        bmi = bmiValue
+        
+        if bmiValue < 18.5 {
+            print("Underweight")
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            print("Normal")
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor.green)
+        } else {
+            print("Overweight")
+            bmi = BMI(value: bmiValue, advice: "Eat less pies", color: UIColor.red)
+        }
     }
 }
