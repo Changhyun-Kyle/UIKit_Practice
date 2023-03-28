1862ccbc332c41d52a07fdd98d7da65d9ba77fdf
Changhyun Kang <changhyun.kyle@gmail.com>
2023-03-28 14:55:57 +0900

    [Progress] Section 15: FlashChat

---
+++ Section_15/FlashChat/FlashChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by 강창현 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
