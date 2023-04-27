b84923ed66af27b559ed2dcc423ee6da81f58093
Changhyun Kang <changhyun.kyle@gmail.com>
2023-04-27 15:01:37 +0900

    [Section 17] Dicee

--- Section_17/Dicee/Dicee/ContentView.swift
+++ Section_17/Dicee/Dicee/ContentView.swift
@@ -8,16 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var leftDiceNumber: Int = 1
+    @State private var rightDiceNumber: Int = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                Spacer()
+                Button {
+                    leftDiceNumber = Int.random(in: 1...6)
+                    rightDiceNumber = Int.random(in: 1...6)
+                } label: {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(.red)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
+
+// MARK: - DiceView
+struct DiceView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .padding()
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
