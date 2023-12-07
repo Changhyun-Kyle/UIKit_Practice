1b8afbb0a16144afa2a95224960ca80cb7f6a5dc
Changhyun Kang <changhyun.kyle@gmail.com>
2023-12-07 20:58:34 +0900

    [Add] Working in Interface Builder

---
+++ AutoLayoutPractice/AutoLayoutPractice/MyView.swift
@@ -0,0 +1,15 @@
+//
+//  MyView.swift
+//  AutoLayoutPractice
+//
+//  Created by 강창현 on 12/7/23.
+//
+
+import UIKit
+
+@IBDesignable
+class MyView: UIView {
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+}
